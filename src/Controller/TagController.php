3119b3e41c1690a7255131d02ac2983ec251d7f3
Dmitry Borisov <nagasadoy@gmail.com>
2023-01-16 12:24:22 +0300

    аргумент резолвинг с использованием библиотеки fusonic

--- src/Controller/TagController.php
+++ src/Controller/TagController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Tag\DTO\CreateTagDTO;
 use App\Service\TagService;
+use Fusonic\HttpKernelExtensions\Attribute\FromRequest;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,7 +13,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class TagController extends AbstractController
 {
     #[Route('/create', name: 'create')]
-    public function create(CreateTagDTO $createTagDTO, TagService $tagService): Response
+    public function create(#[FromRequest] CreateTagDTO $createTagDTO, TagService $tagService): Response
     {
         $tag = $tagService->create($createTagDTO->getName());
 
