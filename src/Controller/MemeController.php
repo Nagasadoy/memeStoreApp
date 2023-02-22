4d5c365fd2332328ce007f14211416891fbe6fba
Dmitry Borisov <nagasadoy@gmail.com>
2023-02-22 16:13:28 +0300

    Получение единичного мема по id

--- src/Controller/MemeController.php
+++ src/Controller/MemeController.php
@@ -3,48 +3,43 @@
 namespace App\Controller;
 
 use App\Attribute\FromRequest;
-use App\Entity\Meme\DTO\CreateMemeDTO;
 use App\Entity\Meme\Meme;
-use App\Entity\Meme\MemeFile;
 use App\Entity\Tag\DTO\AddTagDTO;
 use App\Entity\User\User;
-use App\Repository\MemeFileRepository;
+use App\Repository\MemeRepository;
 use App\Service\MemeService;
 use App\Service\UserService;
-use Doctrine\ORM\EntityManagerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\File\File;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 use Vich\UploaderBundle\Storage\StorageInterface;
 use Vich\UploaderBundle\Templating\Helper\UploaderHelper;
-use Symfony\Component\HttpFoundation\File\UploadedFile;
 
 #[Route('/api/meme', name: 'meme_')]
 class MemeController extends AbstractController
 {
-//    #[Route('/file/create', name: 'file_create', methods: ['POST'])]
-//    public function createFileMeme(
-//        Request $request,
-//        MemeService $memeService,
-//        EntityManagerInterface $entityManager
-//    ): Response {
-//        /** @var File $file */
-//        $file = $request->files->get('file');
-//
-//        $memeFile = new MemeFile($file->getFilename());
-//        $memeFile->setFile($file);
-//
-//        $entityManager->persist($memeFile);
-//        $entityManager->flush();
-//
-//        return $this->json([
-//            'id' => $memeFile->getId(),
-//        ]);
-//    }
+    #[isGranted('IS_AUTHENTICATED_FULLY')]
+    #[Route('/{id}', name: 'by_id', methods: ['GET'])]
+    public function getMeme(int $id, MemeRepository $memeRepository, StorageInterface $storage, Request $request): Response
+    {
+        $meme = $memeRepository->find($id);
+
+        if ($meme === null) {
+            throw new BadRequestHttpException('Нет мема с таким id!');
+        }
+
+        $currentUser = $this->getUser();
+
+        if ($meme->getUser() !== $currentUser) {
+            throw new BadRequestHttpException('Пользователь не является владельцем этого мема!');
+        }
+
+        return $this->json($meme, Response::HTTP_OK, [], ['groups' => ['meme:main', 'tag:main']]);
+    }
 
     #[isGranted('IS_AUTHENTICATED_FULLY')]
     #[Route('/remove/{id}', name: 'remove', methods: ['DELETE'])]
@@ -58,7 +53,7 @@ class MemeController extends AbstractController
         return new Response('');
     }
 
-//    #[isGranted('IS_AUTHENTICATED_FULLY')]
+    #[isGranted('IS_AUTHENTICATED_FULLY')]
     #[Route('/create', name: 'create', methods: ['POST'])]
     public function createMeme(
 /*        #[FromRequest] CreateMemeDTO $createMemeDTO,*/
